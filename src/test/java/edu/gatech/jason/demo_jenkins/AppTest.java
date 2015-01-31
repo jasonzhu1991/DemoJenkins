56f6f9fe3e2bb0d4585dd80ba698698fb7f9ce26
Jason Zhu <jason@starmobileinc.com>
2015-01-31 17:05:25 -0500

    Add everything

--- src/test/java/edu/gatech/jason/demo_jenkins/AppTest.java
+++ src/test/java/edu/gatech/jason/demo_jenkins/AppTest.java
@@ -1,7 +1,5 @@
 package edu.gatech.jason.demo_jenkins;
 
-import java.util.Random;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,11 +11,6 @@ public class AppTest {
 	@Test
 	public void testPass() {
 		Assert.assertTrue(true);
-	}
-
-	@Test
-	public void testRandom() {
-		Assert.assertTrue(new Random().nextBoolean());
 	}
 
 }
